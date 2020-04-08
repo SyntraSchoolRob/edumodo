9bcd6aec8fcdfbca46b7aa1a6d75fc4e6f5f8eb7
robbe <hello@hi.com>
2020-04-08 18:00:26 +0200

    add content in dashboard content section

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,5 +3,48 @@
     Dashboard
 @endsection
 @section('content')
-
+    <div class="col-lg-5">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-warning">Welcome {{ Auth::user()->name }} </h6>
+                </div>
+                <div class="card-body">
+                    <p>Welcome to your dashboard. Find and manage all your data, create & design what you love, update your shop, ship you're orders & analyse you're sales revenue.  </p>
+                    <a rel="nofollow" href="{{route('posts.index')}}">View your products &rarr;</a>
+                </div>
+            </div>
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-success">Overview of you're dashboard</h6>
+                </div>
+                <div class="card-body">
+                    <p>lorem ipsum text ehre</p>
+                    <p class="mb-0">Lorem ipsum text here</p>
+                </div>
+            </div>
+    </div>
+    <div class="col-lg-7">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                <div class="dropdown no-arrow">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                        <div class="dropdown-header">Dropdown Header:</div>
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="chart-area">
+                    <canvas id="myAreaChart"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
