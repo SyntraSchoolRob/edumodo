fc640b6ab5b727e07cb711b129b522f603883e6a
robbe <hello@hi.com>
2020-04-09 14:13:46 +0200

    creation of admin user fields in UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -17,8 +18,14 @@ class UsersTableSeeder extends Seeder
             'is_active'=>1,
             'name'=>'EdumodoAdmin',
             'email'=>'hi@hello.us',
+            'email_verified_at'=>now(),
             'photo_id'=>1,
-            'password'=>bcrypt(12345678)]);
+            'password'=>bcrypt(12345678),
+            'remember_token' => Str::random(10),
+            'created_at'=>now(),
+            'updated_at'=>now(),
+
+        ]);
         factory('App\User',5)->create();
     }
 }
